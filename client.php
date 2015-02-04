c862f855425aca8c44be0742b709f5dad2b9dcd1
Vinicius Cagnotto <viniciuscagnotto@gmail.com>
2015-02-04 15:46:51 -0500

    SOAP example Test
    simple webservice example

---
+++ client.php
@@ -0,0 +1,21 @@
+<pre>
+<?php
+ 
+ini_set("soap.wsdl_cache_enabled", "0");
+ 
+// A seguir você devera informar a URL do webservice.
+$oSoapClient = new SoapClient("http://gatopolisphpclient.azurewebsites.net/service.wsdl");
+ 
+$aOptions = array (
+       "start_debug"=> "1",
+       "debug_port"=> "10000",
+       "debug_host"=> "localhost",
+       "debug_stop"=> "1");
+ 
+foreach($aOptions as $key => $val) {
+        $oSoapClient->__setCookie($key,$val);
+}
+var_dump($oSoapClient->getUser());
+?>
+</pre>
+[<a href=".">Voltar</a>]
